fe133f696428f010fda8c8abaa235c9c5b5a1e77
ElJuanchito <perdomocardenas18@gmail.com>
2023-09-25 14:56:28 -0500

    Angregando gitIgnore

---
+++ workspace/alquilervehiculos/src/main/java/co/edu/uniquindio/alquilervehiculos/model/Alquiler.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.alquilervehiculos.model;
+
+import java.util.Map;
+
+public class Alquiler {
+	private Map<String, Cliente> listaClientes;
+	private Map<String, Vehiculo> listaVehiculos;
+	
+	public boolean verificarCliente(String cedula) {
+		
+	}
+}
