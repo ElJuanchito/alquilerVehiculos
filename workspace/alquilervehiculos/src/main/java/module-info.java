52d46d762eb580f5bd4f35aaa0e484956ccf5ed2
ElJuanchito <perdomocardenas18@gmail.com>
2023-09-30 23:06:55 -0500

    Agregue unas etiquetas para que todo se vea mas lindo

--- workspace/alquilervehiculos/src/main/java/module-info.java
+++ workspace/alquilervehiculos/src/main/java/module-info.java
@@ -1,11 +1,12 @@
 module co.edu.uniquindio.alquilervehiculos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires lombok;
 	requires java.logging;
 
-    opens co.edu.uniquindio.alquilervehiculos.application to javafx.fxml, javafx.controls;
-    exports co.edu.uniquindio.alquilervehiculos.application;
-    
+    opens co.edu.uniquindio.alquilervehiculos.application to javafx.fxml, javafx.controls, javafx.graphics;   
     opens co.edu.uniquindio.alquilervehiculos.controllers to javafx.fxml;
+    
+    exports co.edu.uniquindio.alquilervehiculos.application;
 }
