cf143ddf1a86188c97d588d3dd939c7bd9f82c8e
Breyner-sq <93687667+Breyner-sq@users.noreply.github.com>
2023-10-03 00:20:34 -0500

    funcion extendida
    se agg la ventana de alquilados en una fecha, ademas se creo su controller y la accion del boton que la abre.

--- workspace/alquilervehiculos/src/main/java/co/edu/uniquindio/alquilervehiculos/controllers/MasFuncionesController.java
+++ workspace/alquilervehiculos/src/main/java/co/edu/uniquindio/alquilervehiculos/controllers/MasFuncionesController.java
@@ -1,8 +1,13 @@
 package co.edu.uniquindio.alquilervehiculos.controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import co.edu.uniquindio.alquilervehiculos.application.App;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -28,6 +33,33 @@ public class MasFuncionesController {
 
     @FXML
     private Button btnVehiculosDisponiblesEnFechas;
+
+    @FXML
+    void alquiladosEnUnaFechaEvent(ActionEvent event) {
+    	try {
+			Node nodo = App.loadFXML("alquiladosEnUnaFecha");
+			MenuPrincipalController.getInstance().setCenter(nodo);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
+
+    @FXML
+    void marcaMasAlquiladaEvent(ActionEvent event) {
+    	
+
+    }
+
+    @FXML
+    void totalGanadoEnFechasEvent(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vehiculosDisponiblesEnFechasEvent(ActionEvent event) {
+
+    }
 
     @FXML
     void initialize() {
